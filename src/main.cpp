cc05e144203b9f3d259ccd23f344d6a396e7202f
Josh <jtrendro@gmail.com>
2025-03-12 20:19:23 -0600

    [corect libarys]

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,30 @@
 #include <Arduino.h>
+// #include <TFT_eSPI.h>
+// #include "display.h";
 
-// put function declarations here:
-int myFunction(int, int);
+// // put function declarations here:
+// int myFunction(int, int);
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
-}
+// void setup() {
+//   // put your setup code here, to run once:
+//   int result = myFunction(2, 3);
+//   displayText();
+// }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+// void loop() {
+//   // put your main code here, to run repeatedly:
+// }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
+// // put function definitions here:
+// int myFunction(int x, int y) {
+//   return x + y;
+// }
+
+// TFT_eSPI tft = TFT_eSPI();
+
+// void setup(){
+//   tft.init();
+//   tft.setRotation(1);
+//   tft.fillScreen(TFT_BLACK);
+//   tft.drawString("hellow", 30,30,2);
+// }
